c5078e8dad34e7f7a4a1028e3a2205400f792c61
ImadABID <imad.abied@gmail.com>
2021-08-15 02:35:26 +0200

    implementing list_teammate_register

--- Code_secret_finder/src/list_teammate.c
+++ Code_secret_finder/src/list_teammate.c
@@ -12,3 +12,29 @@ char list_teammate_empty(struct list_teammate *lt){
     return lt->next == NULL;
 }
 
+void list_teammate_register(struct list_teammate *lt, int nbr){
+    char registered = 0;
+    struct list_teammate *lt_pre_i;
+    struct list_teammate *lt_i = lt;
+    while(lt_i->next != NULL){
+        lt_pre_i = lt_i;
+        lt_i = lt_i->next;
+        if(lt_i->number == nbr){
+            lt_i->teammate_occasion_number++;
+            list_teammate_organise(lt_pre_i);
+            registered = 1;
+            break;
+        }
+    }
+    if(!registered){
+        struct list_teammate *teammate = malloc(sizeof(struct list_teammate));
+        teammate->number = nbr;
+        teammate->teammate_occasion_number = 0;
+        teammate->next = lt->next;
+        lt->next = teammate;
+    }
+}
+
+void list_teammate_organise(){
+
+}
